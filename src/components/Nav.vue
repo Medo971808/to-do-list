e346ce5384757feb13ae44f41b7ae507779827ec
MohamedAbedy <hdjdrtrrf@gmail.com>
2025-08-02 02:26:48 +0300

    create upcoming

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,17 +13,17 @@
       <h2 class="text-md font-extrabold font-oswald mt-12">Tasks</h2>
       <section class="w-full px-5 text-md text-gray-600 mt-3">
         <article class="text-center flex justify-between mb-2">
-          <p><i class="fas fa-angles-right mr-2"></i>Upcoming</p>
+          <router-link><i class="fas fa-angles-right mr-2"></i>Upcoming</router-link>
           <p class="text-sm font-semibold bg-gray-200 rounded-md w-6">15+</p>
         </article>
 
         <article class="text-center flex justify-between mb-2">
-          <p><i class="fa-solid fa-list mr-2"></i>Today</p>
+          <router-link><i class="fa-solid fa-list mr-2"></i>Today</router-link>
           <p class="text-sm font-semibold bg-gray-200 rounded-md w-6">8</p>
         </article>
 
-          <p class=" mb-2"><i class="fa-regular fa-calendar mr-2"></i>Calendar</p>
-          <p class=" mb-2"><i class="fas fa-note-sticky mr-2"></i>Sticky Wall</p>
+          <router-link class="block mb-2"><i class="fa-regular fa-calendar mr-2"></i>Calendar</router-link>
+          <router-link class=" mb-2"><i class="fas fa-note-sticky mr-2"></i>Sticky Wall</router-link>
       </section>
 
       <h2 class="text-md font-extrabold font-oswald mt-12">Lists</h2>
