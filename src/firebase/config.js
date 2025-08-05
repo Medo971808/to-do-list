ad811c443e85f9489a02793d19315891391628c8
MohamedAbedy <hdjdrtrrf@gmail.com>
2025-08-05 03:20:04 +0300

    Security Enviroment

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,14 @@
-
 import { initializeApp } from 'firebase/app'
 import { getFirestore, serverTimestamp } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPO2Qmya6_BbVgfyzflZKRrB5pKdAdpMY",
-  authDomain: "todolist-70416.firebaseapp.com",
-  projectId: "todolist-70416",
-  storageBucket: "todolist-70416.appspot.com",
-  messagingSenderId: "908692210630",
-  appId: "1:908692210630:web:6dc60f83b16c5231f828c7"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
