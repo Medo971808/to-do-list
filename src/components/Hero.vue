32ce046f7cf30e67b52755e4320608a14a3f14ec
MohamedAbedy <hdjdrtrrf@gmail.com>
2025-08-06 14:25:24 +0300

    Hero responsive

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,19 @@
 <template>
-    <section class="h-[95vh] w-[70vw] bg-white rounded-3xl flex flex-col items-center justify-center text-center text-black">
-        <h1 class=" font-oswald text-3xl mb-10">Wellcome to ToDoPy</h1>
-        <p class=" w-[40vw] mb-10 text-sm lg:text-lg">
-            A to-do app is a simple, user-friendly digital tool designed to help individuals and teams 
-            organize tasks and manage their daily activities efficiently. Users can create, edit, and prioritize tasks, 
-            set deadlines or reminders, categorize items, and track their progress, all within an intuitive and accessible interface. 
-            These apps are essential for improving productivity, 
-            reducing stress, and ensuring that important responsibilities are not forgotten.
-        </p>
-        <router-link :to="{ name: 'Upcoming'}"
-            class="font-oswald w-[30vw] h-10 text-2xl flex justify-center items-center rounded-lg bg-[#271b67] hover:bg-[#1c173e] text-white">
-            Go to tasks
-        </router-link>
+    <section class="h-full flex items-center">
+        <section class="h-[80vh] lg:h-[95vh] w-[70vw] bg-white rounded-3xl flex flex-col items-center justify-center lg:text-center text-black">
+            <h1 class=" font-oswald text-3xl mb-10">Wellcome to ToDoPy</h1>
+            <p class=" w-[40vw] mb-10 text-sm lg:text-lg">
+                A to-do app is a simple, user-friendly digital tool designed to help individuals and teams 
+                organize tasks and manage their daily activities efficiently. Users can create, edit, and prioritize tasks, 
+                set deadlines or reminders, categorize items, and track their progress, all within an intuitive and accessible interface. 
+                These apps are essential for improving productivity, 
+                reducing stress, and ensuring that important responsibilities are not forgotten.
+            </p>
+            <router-link :to="{ name: 'Upcoming'}"
+                class="font-oswald w-[30vw] h-10 text-2xl flex justify-center items-center rounded-lg bg-[#271b67] hover:bg-[#1c173e] text-white">
+                Go to tasks
+            </router-link>
+        </section>
     </section>
 </template>
 
